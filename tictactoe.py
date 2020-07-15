fe4ed386b7d1a9acfd186d104b1d9b910d2cf57e
Natalia Bustamante Rodriguez <natisaxo@gmail.com>
2020-07-15 18:58:58 +0200

    Make sure that the user's choice is an int.

--- tictactoe.py
+++ tictactoe.py
@@ -1,2 +1,8 @@
 print("   |   |   \n---+---+---\n   |   |   \n---+---+---\n   |   |   ")
-choice = input("Where do you want to play: ")
+choice = None
+while choice is None:
+    try:
+        choice = int(input("Where do you want to play: "))
+    except ValueError:
+        choice = None
+print(type(choice))
