aa28f42f85c728eb4ab9444a43654030a8ebc492
Natalia Bustamante Rodriguez <natisaxo@gmail.com>
2020-07-18 22:17:41 +0200

    Ask for input outside of the try.
    This highlights that the try is about the cast into an integer

--- tictactoe.py
+++ tictactoe.py
@@ -1,8 +1,9 @@
 print(" 1 | 2 | 3 \n---+---+---\n 4 | 5 | 6 \n---+---+---\n 7 | 8 | 9 ")
 choice = None
 while choice is None:
+    choice = input("Where do you want to play: ")
     try:
-        choice = int(input("Where do you want to play: "))
+        choice = int(choice)
     except ValueError:
         choice = None
 print(type(choice))
