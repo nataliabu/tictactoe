369669009216f36629f7bccad7a9d00d4f5c8dd3
Natalia Bustamante Rodriguez <natisaxo@gmail.com>
2020-07-20 21:01:43 +0200

    Define winning end of the game

--- tictactoe.py
+++ tictactoe.py
@@ -24,4 +24,15 @@ while True:
 
     grid_content[choice-1] = players[current_player]
 
+    if (grid_content[0] == grid_content[1] and grid_content[1] == grid_content[2]
+        or grid_content[3] == grid_content[4] and grid_content[4] == grid_content[5]
+        or grid_content[6] == grid_content[7] and grid_content[7] == grid_content[8]
+        or grid_content[0] == grid_content[3] and grid_content[3] == grid_content[6]
+        or grid_content[1] == grid_content[4] and grid_content[4] == grid_content[7]
+        or grid_content[2] == grid_content[5] and grid_content[5] == grid_content[8]
+        or grid_content[0] == grid_content[4] and grid_content[4] == grid_content[8]
+        or grid_content[2] == grid_content[4] and grid_content[4] == grid_content[6]
+    ):
+        print("Congrats player {}, you won".format(players[current_player]))
+
     current_player = (current_player + 1) % 2
